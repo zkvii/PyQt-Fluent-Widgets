f0d6e0e08cc4376c5c102eba174056a4f7f8416e
zhiyiYo <1319158137@qq.com>
2023-03-15 21:59:27 +0800

    添加图标抽象类 `FluentIconBase`

--- qfluentwidgets/components/widgets/icon_widget.py
+++ qfluentwidgets/components/widgets/icon_widget.py
@@ -4,17 +4,17 @@ from typing import Union
 from PySide2.QtGui import QIcon, QPainter
 from PySide2.QtWidgets import QWidget
 
-from ...common.icon import FluentIcon, drawIcon
+from ...common.icon import FluentIconBase, drawIcon
 
 
 class IconWidget(QWidget):
     """ Icon widget """
 
-    def __init__(self, icon: Union[str, QIcon, FluentIcon], parent=None):
+    def __init__(self, icon: Union[str, QIcon, FluentIconBase], parent=None):
         super().__init__(parent=parent)
         self.icon = icon
 
     def paintEvent(self, e):
         painter = QPainter(self)
-        painter.setRenderHints(QPainter.Antialiasing|QPainter.SmoothPixmapTransform)
+        painter.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform)
         drawIcon(self.icon, painter, self.rect())
