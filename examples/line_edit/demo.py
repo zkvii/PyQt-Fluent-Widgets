a239980017bf7969f5df77fc699edf3ec637fea7
zhiyiYo <1319158137@qq.com>
2023-03-29 10:19:29 +0800

    修复 LineEdit 右边距过大的问题

--- examples/line_edit/demo.py
+++ examples/line_edit/demo.py
@@ -3,7 +3,7 @@ import sys
 
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import QApplication, QWidget
-from qfluentwidgets import LineEdit
+from qfluentwidgets import LineEdit, PushButton
 
 
 class Demo(QWidget):
@@ -11,8 +11,10 @@ class Demo(QWidget):
     def __init__(self):
         super().__init__()
         self.lineEdit = LineEdit('', self)
+        self.button = PushButton('按钮', self)
         self.resize(500, 500)
-        self.lineEdit.move(150, 220)
+        self.lineEdit.move(110, 220)
+        self.button.move(320, 220)
         self.lineEdit.resize(200, 33)
         self.lineEdit.setClearButtonEnabled(True)
 
