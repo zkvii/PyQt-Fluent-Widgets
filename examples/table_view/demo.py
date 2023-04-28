ed69e9a9255f23990c963c981b2dcc5d6162d5e3
zhiyiYo <1319158137@qq.com>
2023-04-28 17:51:06 +0800

    修复表格组件无法多选的问题

--- examples/table_view/demo.py
+++ examples/table_view/demo.py
@@ -2,7 +2,7 @@
 import sys
 
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QApplication, QHeaderView, QTableWidgetItem, QWidget, QHBoxLayout
+from PySide2.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QWidget, QHBoxLayout
 
 from qfluentwidgets import TableWidget, setTheme, Theme, TableView
 
@@ -59,13 +59,14 @@ class Demo(QWidget):
         self.tableView.verticalHeader().hide()
         self.tableView.resizeColumnsToContents()
         self.tableView.setHorizontalHeaderLabels(['标题', '艺人', '专辑', '年份', '时长'])
+        self.tableView.resizeColumnsToContents()
         # self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         # self.tableView.setSortingEnabled(True)
 
         self.setStyleSheet("Demo{background: rgb(249, 249, 249)} ")
         self.hBoxLayout.setContentsMargins(0, 0, 0, 0)
         self.hBoxLayout.addWidget(self.tableView)
-        self.resize(625, 600)
+        self.resize(625, 700)
 
 
 if __name__ == "__main__":
